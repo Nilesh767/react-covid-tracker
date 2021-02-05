75c2ac21283b1f73e9f06e17922e529239bff61c
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-02-05 15:13:16 +0530

    Added Title and Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import "./App.css";
 
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App">
-    Header
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("WorldWide");
 
-    Title + Dropbox
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
 
+  const onChangeCountry = (event) => {
+    const countryCode = event.target.value;
+    console.log(countryCode);
+    setCountry(countryCode);
+  };
+
+  return (
+    <div className="App">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onChangeCountry} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+
+      <div className="app_stats">
+        Infoboxes title="corona cases"
+        Infoboxes title="corona recoveries"
+        Infoboxes title="corona cases"
+      </div>
+
+  {
+  
+  /* 
     Info
 
     Table
     Graph
 
-    Map   
-  </div>;
+    Map    */
+  }
+    </div>
+  );
 }
 
 export default App;
